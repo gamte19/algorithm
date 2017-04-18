2042a154961a6697d34715aac2704ef619063a88
gamte19 <gamte19@gmail.com>
2017-04-19 01:07:16 +0200

    New algorithms

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
+import data_structure.Stack;
+
 import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] sortedList = {15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14};
+        int[] sortedList = {15, 16, 19, 20, 25, 30, 34, 41, 52, 74, 100, 114};
         BinarySearch binarySearch = new BinarySearch();
-        System.out.println("Normal " + binarySearch.search(sortedList, 5));
-        System.out.println("Recursive " + binarySearch.binarySearchRecursive(sortedList, 5, 0, sortedList.length));
+        System.out.println("Normal " + binarySearch.search(sortedList, 25));
+        System.out.println("Recursive " + binarySearch.binarySearchRecursive(sortedList, 25, 0, sortedList.length));
 
         System.out.println("-----------------------------");
-        int bubbleList[] = {15, 3, 9, 19, 7, 8, 1, 5, 4};
+        int bubbleList[] = {15, 3, 9, 19,19,19,7, 7, 8, 1, 5, 4};
         System.out.println(Arrays.toString(bubbleList));
 
         BubbleSort bubbleSort = new BubbleSort();
@@ -17,7 +19,7 @@ public class Main {
         System.out.println(Arrays.toString(bubbleList));
 
         System.out.println("-----------------------------");
-        int mergeList[] = {15, 3, 9, 19, 7, 8, 1, 5, 4};
+        int mergeList[] = {15, 3, 9, 19,19,19,7, 7, 8, 1, 5, 4};
         System.out.println(Arrays.toString(mergeList));
         Mergesort mergesort = new Mergesort();
         mergesort.sort(mergeList);
@@ -25,11 +27,19 @@ public class Main {
 
         System.out.println("-----------------------------");
         Quicksort quicksort = new Quicksort();
-        int quickList[] = {15, 3, 9, 19, 7, 8, 1, 5, 4};
+        int quickList[] = {15, 3, 9, 19,19,19,7, 7, 8, 1, 5, 4};
         System.out.println(Arrays.toString(quickList));
         quicksort.sort(quickList);
         System.out.println(Arrays.toString(quickList));
 
+        Stack<Integer> stack = new Stack();
+        stack.put(3);
+        stack.put(2);
+        stack.put(4);
+        stack.put(5);
+        System.out.println(stack.toString());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
 
     }
 }
