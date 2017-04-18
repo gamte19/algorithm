6bd3fdc1e851cd2944d9db608b37c7a4aaa4157e
gamte19 <gamte19@gmail.com>
2017-04-19 01:06:29 +0200

    Correct name

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -4,20 +4,20 @@
 public class BubbleSort {
 
 
-    public int[] bubbleSor(int[] crunchifyArr) {
-        int temp;
-        for (int i = 0; i < crunchifyArr.length - 1; i++) {
-            for (int j = 1; j < crunchifyArr.length - i; j++) {
-                if (crunchifyArr[j - 1] > crunchifyArr[j]) {
-                    temp = crunchifyArr[j - 1];
-                    crunchifyArr[j - 1] = crunchifyArr[j];
-                    crunchifyArr[j] = temp;
-                }
-            }
-        }
-        System.out.println("----------------------");
-        return crunchifyArr;
-    }
+  public int[] bubbleSor(int[] values) {
+      int temp;
+      for (int i = 0; i < values.length-1 ; i++) {
+          for (int j = 1; j < values.length-i; j++) {
+              if (values[j-1] > values[j]){
+                  temp = values[j-1];
+                  values[j-1] = values[j];
+                  values[j] = temp;
+              }
+          }
+      }
+
+      return values;
+  }
 
 
 
